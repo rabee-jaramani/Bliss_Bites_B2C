da46fac9460bf4a7826bfd8adbbaab4abfb3efab
Rabee Aljaramani <rabeejermani@gmail.com>
2024-06-03 14:38:52 +0400

    j

--- src/footer/Footer.tsx
+++ src/footer/Footer.tsx
@@ -4,9 +4,15 @@ import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 export default function Footer() {
   return (
     <div className='footer-container'>
-        <p className='footer-powered-p'>All Products are made in a facility that handles nuts, dairy and eggs
-        Advance pre-payment via online transfer is required to confirm the order.  </p>
-        <p className='footer-powered-p'>Remaining balance to be settled cash upon delivery </p>
+
+
+<p className='footer-powered-p'> DISCLAIMER: </p>
+
+        <p className='footer-powered-p'>ALL PRODUCTS ARE MADE IN A FACILITY THAT HANDLES NUTS, DAIRY AND EGGS<br/>
+ADVANCE PRE- PAYMENT VIA ONLINE TRANSFER<br/>
+BALANCE - CASH ON DELIVERY<br/>
+FOR DELIVERY CALL OR WHATSAAP : 0508377409<br/>
+GET SOCIAL: @BLISSBITESGCC  </p>
         <p className='footer-powered-p'> <strong>Proudly made in UAE </strong></p>
     </div>
   )
